7cecbba58ae0def778dc36e1172314ae05e29292
Maxi Burastero <maxicc4@gmail.com>
2017-09-28 11:02:00 -0300

    Si se encuentra un archivo que tiene el formato de nombre esperado se lo mueve de carpeta

--- grupo01/bin/daemon.sh
+++ grupo01/bin/daemon.sh
@@ -2,11 +2,20 @@
 
 #TODO: chequear que se haya inicializado previamente el ambiente
 
-STOP=false;
-DIRABUS="pruebas_daemon";  # Deberian venir seteada de algun lado
+#MY_PATH="`dirname \"$0\"`"
+#echo "$MY_PATH"
+
+STOP=false
+DIRABUS="pruebas_daemon/dirabus"  # Deberian venir seteada de algun lado
 
 while [ "$STOP" = false ]; do
-	echo "Archivos encontrados:"
-	find $DIRABUS -name '[0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][01][0-9][0123][0-9].txt'
-	sleep 2;
+	FILE="$(find $DIRABUS -name '[0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][01][0-9][0123][0-9].txt' -print -quit)"
+	if [ "$FILE" != "" ]; then
+		echo "Archivo encontrado: $FILE"
+		mv $FILE pruebas_daemon/aceptados
+	else
+		echo "No se encontro ningun archivo"
+	fi
+
+	sleep 3
 done
