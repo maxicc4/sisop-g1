f75800829333243a3bef1be1432cc488019a4cdb
MARIA FLORENCIA PRADO <mfprado@icloud.com>
2017-10-04 15:16:45 -0300

    agrego scrips que chequean inicializacion del sistema

---
+++ grupo01/bin/ambienteInicializado.sh
@@ -0,0 +1,3 @@
+#Armar script que devuelva 1 si el sistema fue inicializado
+# 0 si nunca se inicializo.
+# Este se usará por el PREPARADOR, DEMONIO, VERIFICADOR Y LISTADOR
---
+++ grupo01/bin/sistemaInicializadoOk.sh
@@ -0,0 +1,3 @@
+# Armar script que devuelva 1 si el sistema esta inicializado correctamente
+# 0 si hay errores en la inicializacion
+# Este se usará por el PREPARADOR, DEMONIO, VERIFICADOR Y LISTADOR
