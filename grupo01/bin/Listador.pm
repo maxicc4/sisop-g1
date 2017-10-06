377b089f87c440b7df70a12a123853ba2ecdd235
MARIA FLORENCIA PRADO <mfprado@icloud.com>
2017-10-06 01:22:58 -0300

    Modo automatico terminado, invocar con perl listador -a

--- grupo01/bin/Listador.pm
+++ grupo01/bin/Listador.pm
@@ -1,35 +1,89 @@
 # listadorAutomatico.pm
 package Listador;
+use Time::Piece;
+use Time::Seconds;
 
-# $files_processed=$ENV{'file_proccessed'};
+
+# $files_validated=$ENV{'file_validated'};
+# $files_reports=$ENV{'files_reports'};
+$files_validated = '../files_validated/';
+$files_report = '../files_report/';
 
 
 sub automatico {
 	print "INICIALIZADO LISTADOR MODO AUTOMATICO\n";
 	 
-	my $dir = '../files_validated/';
+	#Busco nombre del ultimo archivo validado en files_validated
+    opendir(FILES_VALIDATED, $files_validated) or die $!;
 
-    opendir(DIR, $dir) or die $!;
+    my @files = readdir(FILES_VALIDATED);
 
-    my @files = readdir(DIR);
+    $last_validated_file_dir = $files_validated . @files[-1];
 
-    $lastProccessed = $dir . @files[-1];
-    
-    closedir(DIR);
+    closedir(FILES_VALIDATED);
+
+    #Defino nombre del nuevo reporte
+    opendir(REPORTS, $files_report) or die $!;
+	@files =  readdir(REPORTS);
+	$nro_seq = @files -2; #Definido por la cantidad de reportes realizados
+	closedir(REPORTS);
 
 
-	if (open(my $fh, '<:encoding(UTF-8)', $lastProccessed)) {
-	  while (my $row = <$fh>) {
-	    chomp $row;
-	    print "$row\n";
-	  }
-	} else {
-	  warn "Could not open file '$filename' $!";
-}
+    # Creo archivo del nuevo reporte
+    $report_dir = $files_report . "plasticos_distribucion_" . $nro_seq;
+   
+	open (REPORT_FILE, ">>", $report_dir) or die $!;
+
+
+	open (LAST_VALIDATED_FILE, $last_validated_file_dir) or die "ERROR: No se pudo abrir el archivo de contratos\n";
+	
+	#Defino condicion de distribucion
+	while (<LAST_VALIDATED_FILE>)
+	{	
+		chomp($_);
+		@reg=split(";",$_);
+
+
+		my $today = Time::Piece->new;
+		my $to_date = Time::Piece->strptime(@reg[16], '%d/%m/%Y');
+
+		if (@reg[2] eq 'BAJA') {
+			@reg[6] = "NO DISTRIBUIR, la cuenta esta dada de BAJA";
+		}
+		elsif (@reg[2] eq 'CTX') {
+			@reg[6] = "NO DISTRIBUIR, la cuenta es CONTENCIOSA";
+		}
+		elsif (@reg[2] eq 'JUD') {
+			@reg[6] = "NO DISTRIBUIR, la cuenta es JUDICIAL";
+		}
+		elsif (@reg[5] == 1){
+			@reg[6] = "NO DISTRIBUIR, la tarjeta fue BLOQUEADA";	
+		}
+		elsif ($to_date <= $today){
+			@reg[6] = "NO DISTRIBUIR, tarjeta VENCIDA";	
+		}
+		elsif (($today + ONE_DAY*10) >= $to_date){
+			@reg[6] = "NO DISTRIBUIR, VENTANA de distribucion insuficiente";
+		}
+		elsif (@reg[4] == 1){
+			@reg[6] = "DISTRIBUCION URGENTE";
+		}
+		else{
+			@reg[6] = "DISTRIBUCION ESTANDAR";
+		}
+
+		@reg[7] = $today->strftime('%d/%m/%Y');;
+		@reg[8] = 'LISTADOR';
+		printf REPORT_FILE join( ';', @reg );
+		printf REPORT_FILE "\n";
+	
+	}
+
+
+	close(LAST_VALIDATED_FILE);
+	close(REPORT_FILE);
 
     exit 0;
-
-
 
 }
 
@@ -41,7 +95,8 @@ sub manual {
 		print "Modo ayuda @_[1] \n";
 	}
 
-	print "Seleccion de input: \n
+	print "Ingrese archivo/s de input: \n
+	Puede ingresar: \n
 	- un archivo especifico de plasticos_emitidos \n
 	- un archivo de plasticos_distribucion \n
 	- varios archivos especificos (de emitidos, de distribucion o de ambos)\n
