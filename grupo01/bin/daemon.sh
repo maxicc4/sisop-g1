40d7e2c4b4aa5b0d7d85663282ff513ad815dd76
Maxi Burastero <maxicc4@gmail.com>
2017-10-07 20:26:19 -0300

    Se agregan varias validaciones en el demonio

--- grupo01/bin/daemon.sh
+++ grupo01/bin/daemon.sh
@@ -5,11 +5,13 @@
 #MY_PATH="`dirname \"$0\"`"
 #echo "$MY_PATH"
 
-STOP="false"
 # Deberian venir seteadas de algun lado
 DIRABUS="pruebas_daemon/dirabus"
 DIRACCEPTED="../files_accepted"
 DIRREJECTED="../files_rejected"
+
+STOP="false"
+CYCLE=0
 
 # Se obtiene de a un archivo por vez
 getFile()
@@ -17,26 +19,94 @@ getFile()
 	FILE="$(find $DIRABUS -type f -print -quit)"
 }
 
-verifyFile()
+verifyFormatNameFile()
 {
-	VERIFIED="false"
-	VERIFIED="$(echo $FILE | sed "/[\/ ][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][01][0-9][0123][0-9].txt/c\true")"
+	VERIFIED="$(echo $FILE | sed "/[\/ ][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].txt/c\true")"
+	if [ $VERIFIED != "true" ]; then
+		VERIFIED="false"
+	fi
+}
+
+verifyFormatDate()
+{
+	VERIFIED="$(echo $FILE | sed -r "/[\/ ][0-9][0-9][0-9]_([0][0][0][1-9]|[0][0][1-9][0-9]|[0][1-9][0-9][0-9]|[1-9][0-9][0-9][0-9])((01|03|05|07|08|10|12)([0][1-9]|[12][0-9]|[3][01])|(04|06|09|11)([0][1-9]|[12][0-9]|[3][0])|02([0][1-9]|[12][0-9])).txt/c\true")"
+	if [ $VERIFIED != "true" ]; then
+		VERIFIED="false"
+	fi
+}
+
+verifyDateAgainstCurrentDate()
+{
+	DATE="$(echo -n $FILE | tail -c -12 | cut -c-8)"
+	CURRENTDATE="$(date "+%Y%m%d")"
+	RESULT="$(echo "$DATE <= $CURRENTDATE" | bc)"
+
+	if [ $RESULT = 1 ]; then
+		VERIFIED="true"
+	else
+		VERIFIED="false"
+	fi
+}
+
+verifyEmptyFile()
+{
+	if [ -s $FILE ]; then
+		VERIFIED="true"
+	else
+		VERIFIED="false"
+	fi
+}
+
+verifyRegularFile()
+{
+	typeFile="$(file -i $FILE | cut -d' ' -f2)"
+	if [ $typeFile = "text/plain;" ]; then
+		VERIFIED="true"
+	else
+		VERIFIED="false"
+	fi
 }
 
 while [ "$STOP" = "false" ]; do
+	CYCLE=$((CYCLE+1))
+	echo "------LOG------ Ciclo Numero $CYCLE"
 	getFile
 	while [ "$FILE" != "" ]; do
-		verifyFile
-		if [ $VERIFIED = "true" ]; then
-			echo "Archivo aceptado: $FILE"
-			mv $FILE $DIRACCEPTED
-		else
-			echo "Archivo rechazado: $FILE"
+		verifyFormatNameFile
+		if [ $VERIFIED = "false" ]; then
+			echo "------LOG------ Novedad rechazada $FILE: Nombre de archivo invalido"
 			mv $FILE $DIRREJECTED
+		else
+			verifyFormatDate
+			if [ $VERIFIED = "false" ]; then
+				echo "------LOG------ Novedad rechazada $FILE: Fecha invalida"
+				mv $FILE $DIRREJECTED
+			else
+				verifyDateAgainstCurrentDate
+				if [ $VERIFIED = "false" ]; then
+					echo "------LOG------ Novedad rechazada $FILE: Fecha adelantada"
+					mv $FILE $DIRREJECTED
+				else
+					verifyEmptyFile
+					if [ $VERIFIED = "false" ]; then
+						echo "------LOG------ Novedad rechazada $FILE: Archivo vacio"
+						mv $FILE $DIRREJECTED
+					else
+						verifyRegularFile
+						if [ $VERIFIED = "false" ]; then
+							echo "------LOG------ Novedad rechazada $FILE: Tipo de archivo invalido"
+							mv $FILE $DIRREJECTED
+						else
+							echo "------LOG------ Novedad aceptada $FILE"
+							mv $FILE $DIRACCEPTED
+						fi
+					fi
+				fi
+			fi
 		fi
+
 		getFile
 	done
-	echo "No se encontro ningun archivo"
 
 	sleep 3
 done
